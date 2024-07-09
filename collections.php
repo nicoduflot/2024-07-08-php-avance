43d6f706c2db2ca6a7493b2daf8025238989385b
unknown <nduflot@dawan.fr>
2024-07-09 12:31:55 +0200

    collections pt 1

--- collections.php
+++ collections.php
@@ -1,6 +1,7 @@
 <?php
 require_once './vendor/autoload.php';
 use Utils\Tools;
+use Doctrine\Common\Collections\ArrayCollection;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -36,6 +37,72 @@ use Utils\Tools;
                 <p>
                     La doc complète de ArrayCollection se trouve <a href="https://www.doctrine-project.org/projects/doctrine-collections/en/stable/index.html" target="_blank">Ici</a>
                 </p>
+                <?php
+                /*
+                Pour utiliser les collections, il faut créer une instance de la classe Arraycollection
+                si on ne précise pas le use, 
+                $collection = new Doctrine\Common\Collections\ArrayCollection();
+                */
+                /* Création de l'objet collection */
+                $collection = new ArrayCollection();
+                Tools::prePrint($collection);
+                /* on ajoute des éléments à la collection avec la méthode add() */
+                Tools::prePrint($collection);
+                $collection->add('Nicolas Duflot');
+                $collection->add('Bernie Sanders');
+                $collection->add('Fred Flintstone');
+                Tools::prePrint($collection);
+                /* supprimer un élément de la collection */
+                $collection->remove(2);
+                Tools::prePrint($collection);
+                $collection->removeElement('Nicolas Duflot');
+                Tools::prePrint($collection);
+                $collection->add('Nicolas Duflot');
+                $collection->add('Fred Flintstone');
+                /* ajouter un élément avec une clef spécifique */
+                $recherche = $collection->contains('Nicolas Duflot');
+                Tools::prePrint($recherche);
+                $collection->set('Jeu', 'Fallout 4');
+                Tools::prePrint($collection);
+                $recherche = $collection->contains('Momo');
+                Tools::prePrint($recherche);
+                Tools::prePrint($collection->count());
+                Tools::prePrint($collection->get('Jeu'));
+                $collection->set('Jeu', 'Fallout');
+                Tools::prePrint($collection);
+                Tools::prePrint($collection->current());
+                $collection->next();
+                Tools::prePrint($collection->current());
+                $collection->next();
+                Tools::prePrint($collection->current());
+                $collection->first();
+                Tools::prePrint($collection->current());
+                $collection->last();
+                Tools::prePrint($collection->current());
+                $collection->first();
+                for($i = 0; $i < $collection->count(); $i = $i + 1){
+                    print_r($collection->get($collection->key()));
+                    echo '; ';
+                    $collection->next();
+                }
+
+                Tools::prePrint($collection->current());
+                $mappedCollection = $collection->map(function($element){
+                    Tools::prePrint($element);
+                    return $element.' toto';
+                });
+                Tools::prePrint($mappedCollection);
+                Tools::prePrint($collection);
+
+                $collection2 = new ArrayCollection([1, 2, 3]);
+                Tools::prePrint($collection2);
+
+                $mappedCollection2 = $collection2->map(function($value) {
+                    return $value + 1;
+                }); // [2, 3, 4]
+
+                Tools::prePrint($mappedCollection2);
+                ?>
             </article>
         </section>
     </main>
