12f8385f88acd351fc05578bb3c7003d250333c2
unknown <nduflot@dawan.fr>
2024-07-12 10:40:44 +0200

    préparation formulaire enreg compte et redirection si url mauvais

--- creaCompte.php
+++ creaCompte.php
@@ -1,5 +1,6 @@
 <?php
 require_once './vendor/autoload.php';
+
 use Utils\Tools;
 ?>
 <!DOCTYPE html>
@@ -27,106 +28,134 @@ use Utils\Tools;
                 <header>
                     <h2>Création d'un compte</h2>
                 </header>
-                    <h3>Les compte suivant a été enregistré : </h3>
+                <?php
+                if (isset($_POST['type'])) {
+                    /* traitement des enristrement de comptes */
+                ?>
+                    <h3>Le compte suivant a été enregistré : </h3>
                     <p>
                         <?php
                         ?>
                     </p>
                     <p>
-                    <a href="./classesetpdo.php"><button class="btn btn-outline-secondary btn-small" type="button">Retour à la page des comptes</button></a>
+                        <a href="./classesetpdo.php"><button class="btn btn-outline-secondary btn-small" type="button">Retour à la page des comptes</button></a>
                     </p>
-                        <form method="post">
-                            <fieldset class="form-control my-2">
-                                <legend>
-                                    Détenteur du compte
-                                </legend>
-                                <div class="row my-2">
-                                    <div class="col-lg-6">
-                                        <label for="nom">Nom</label>
-                                        <input type="text" class="form-control" name="nom" id="nom" />
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <label for="prenom">Prénom</label>
-                                        <input type="text" class="form-control" name="prenom" id="nom" />
-                                    </div>
+                <?php
+                } else {
+                    $typeCompte = (isset($_GET['typecompte'])) ? $_GET['typecompte'] : null;
+                ?>
+                    <form method="post">
+                        <fieldset class="form-control my-2">
+                            <legend>
+                                Détenteur du compte
+                            </legend>
+                            <div class="row my-2">
+                                <div class="col-lg-6">
+                                    <label for="nom">Nom</label>
+                                    <input type="text" class="form-control" name="nom" id="nom" />
                                 </div>
-                            </fieldset>
-                            <fieldset class="form-control my-2">
-                                <legend>Agence</legend>
-                                <div class="row my-2">
-                                    <div class="col-lg-6">
-                                        <label for="numagence">Numéro d'agence</label>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <input type="text" class="form-control" name="numagence" id="numagence" />
-                                    </div>
+                                <div class="col-lg-6">
+                                    <label for="prenom">Prénom</label>
+                                    <input type="text" class="form-control" name="prenom" id="nom" />
                                 </div>
-                            </fieldset>
-                            <fieldset class="form-control my-2">
-                                <legend>
-                                    Détails du compte
-                                </legend>
-                                <div class="row my-2">
-                                    <div class="col-lg-6">
-                                        <label for="type">Numéro d'agence</label>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <input type="text" name="type" id="type" value="<?php echo $_GET['typecompte'] ?>" readonly />
-                                    </div>
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-control my-2">
+                            <legend>Agence</legend>
+                            <div class="row my-2">
+                                <div class="col-lg-6">
+                                    <label for="numagence">Numéro d'agence</label>
                                 </div>
-                                        <div class="row my-2">
-                                            <div class="col-lg-6">
-                                                <label for="numcarte">Numéro de carte</label>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <input type="text" readonly class="form-control" name="numcarte" id="numcarte" value="<?php echo $numcarte ?>" />
-                                            </div>
+                                <div class="col-lg-6">
+                                    <input type="text" class="form-control" name="numagence" id="numagence" />
+                                </div>
+                            </div>
+                        </fieldset>
+                        <fieldset class="form-control my-2">
+                            <legend>
+                                Détails du compte
+                            </legend>
+                            <div class="row my-2">
+                                <div class="col-lg-6">
+                                    <label for="type">Type compte</label>
+                                </div>
+                                <div class="col-lg-6">
+                                    <input type="hidden" name="type" id="type" value="<?php echo $typeCompte ?>" readonly />
+                                    <?php echo $typeCompte ?>
+                                </div>
+                            </div>
+                            <?php
+                            switch ($typeCompte) {
+                                case 'Compte':
+                                    break;
+                                case 'CompteCheque':
+                            ?>
+                                    <div class="row my-2">
+                                        <div class="col-lg-6">
+                                            <label for="numcarte">Numéro de carte</label>
                                         </div>
-                                        <div class="row my-2">
-                                            <div class="col-lg-6">
-                                                <label for="codepin">Code secret</label>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <input type="text" readonly class="form-control" name="codepin" id="codepin" value="<?php echo $codepin ?>" />
-                                            </div>
+                                        <div class="col-lg-6">
+                                            <input type="text" readonly class="form-control" name="numcarte" id="numcarte" value="<?php echo $numcarte ?>" />
                                         </div>
-                                        <div class="row my-2">
-                                            <div class="col-lg-6">
-                                                <label for="taux">Taux d'intérêts</label>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <select class="form-select" name="taux" id="taux">
-                                                    <option selected>Choisir le taux d'intéret</option>
-                                                    <option value="0.015">1.5%</option>
-                                                    <option value="0.03">3%</option>
-                                                    <option value="0.05">5%</option>
-                                                </select>
-                                            </div>
+                                    </div>
+                                    <div class="row my-2">
+                                        <div class="col-lg-6">
+                                            <label for="codepin">Code secret</label>
                                         </div>
+                                        <div class="col-lg-6">
+                                            <input type="text" readonly class="form-control" name="codepin" id="codepin" value="<?php echo $codepin ?>" />
+                                        </div>
+                                    </div>
+                                <?php
+                                    break;
+                                case 'CompteInteret':
                                 ?>
-                                <div class="row my-2">
-                                    <div class="col-lg-6">
-                                        <label for="solde">Solde</label>
+                                    <div class="row my-2">
+                                        <div class="col-lg-6">
+                                            <label for="taux">Taux d'intérêts</label>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <select class="form-select" name="taux" id="taux">
+                                                <option selected>Choisir le taux d'intéret</option>
+                                                <option value="0.015">1.5%</option>
+                                                <option value="0.03">3%</option>
+                                                <option value="0.05">5%</option>
+                                            </select>
+                                        </div>
                                     </div>
-                                    <div class="col-lg-6">
-                                        <input type="number" class="form-control" name="solde" id="solde" />
-                                    </div>
+                                <?php
+                                    break;
+                                default:
+                                ?>
+                                    <script>
+                                        document.location.href = "./classesetpdo.php";
+                                    </script>
+                            <?php
+                                    exit();
+                            }
+                            ?>
+                            <div class="row my-2">
+                                <div class="col-lg-6">
+                                    <label for="solde">Solde</label>
                                 </div>
-                            </fieldset>
-                            <p>
-                                <button class="btn btn-outline-success btn-small" type="submit">
-                                    Créer et enregistrer le compte
-                                </button>
-                                <button class="btn btn-outline-warning btn-small" type="reset">
-                                    Vider le formulaire
-                                </button>
-                                <a href="./classesetpdo.php"><button class="btn btn-outline-secondary btn-small" type="button">Annuler</button></a>
-                            </p>
-                        </form>
+                                <div class="col-lg-6">
+                                    <input type="number" class="form-control" name="solde" id="solde" />
+                                </div>
+                            </div>
+                        </fieldset>
                         <p>
-                            Oups !<br />
-                            <a href="./classesetpdo.php">Revenir en arrière</a>
+                            <button class="btn btn-outline-success btn-small" type="submit">
+                                Créer et enregistrer le compte
+                            </button>
+                            <button class="btn btn-outline-warning btn-small" type="reset">
+                                Vider le formulaire
+                            </button>
+                            <a href="./classesetpdo.php"><button class="btn btn-outline-secondary btn-small" type="button">Annuler</button></a>
                         </p>
+                    </form>
+                <?php
+                }
+                ?>
             </article>
 
         </section>
